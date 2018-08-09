50516b27aeaa5da261f2f78d7ddd4171bb6d7106
Woo Jia Hao <woojiahao1234@gmail.com>
2018-08-09 11:44:40 +0800

    added checking for the attribute dictionary if the user supplies one for the writing operation

--- database/writer.py
+++ database/writer.py
@@ -3,6 +3,7 @@ import re
 from pandas import DataFrame
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists
 
 
 class Writer:
@@ -12,11 +13,28 @@ class Writer:
 		if not conn_str_valid[0]:
 			raise Exception(conn_str_valid[1])
 
+		if not database_exists(self.conn_str):
+			raise Exception(f'Database: {self.conn_str[self.conn_str.rfind("/") + 1 : ]} does not exist')
+
 		self.engine = create_engine(conn_str)
 		self.Base = declarative_base(bind=self.engine)
 		self.metadata = self.Base.metadata
 
-	def __verify_connection__(self):
+		print('Initialized!')
+
+	def write(self, tablename: str, data_frame: DataFrame, attr_dict: dict = None):
+		if attr_dict is not None:
+			if '__tablename__' not in attr_dict.keys():
+				print(f'No __tablename__ attribute found, using the one supplied: {tablename}')
+				attr_dict['__tablename__'] = tablename
+
+			num_cols = data_frame.shape[1]
+			if len(attr_dict) != num_cols + 1:
+				raise Exception(f'Invalid number of columns, data frame requires {num_cols} columns, was given {len(attr_dict) - 1} columns')
+
+		print('Writing!')
+
+	def __verify_connection__(self) -> tuple:
 		pattern = '^\w+:\/\/\w+:\w+\@\w+:\d+\/\w+$'
 		if re.match(pattern, self.conn_str) is None:
 			return False, 'Invalid format'
