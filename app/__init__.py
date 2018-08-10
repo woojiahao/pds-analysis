aa6db37569acf80a686ad3fd47dbd9ec36aced96
Woo Jia Hao <woojiahao1234@gmail.com>
2018-08-10 15:53:11 +0800

    bug fix

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
-from database.database_manager import Manager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.DATABASE_CONNECTION_STRING
 db = SQLAlchemy(app)
-manager = Manager()
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,4 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-	manager.load_data()
 	return render_template('base.html')
