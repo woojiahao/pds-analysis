6970c71b5c51d351d6493ef2f4762a15d48f5f12
Woo Jia Hao <woojiahao1234@gmail.com>
2018-08-10 21:22:22 +0800

    began making links

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,8 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
 @app.route('/home')
-def index():
+def home():
 	return render_template('home.html')
 
 @app.route('/about')
