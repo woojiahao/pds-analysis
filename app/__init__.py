406306279c45112471631b259b5292ebc4a403b8
Woo Jia Hao <woojiahao1234@gmail.com>
2018-08-10 18:30:12 +0800

    suppress track modification warning for sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import Config
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.DATABASE_CONNECTION_STRING
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from app import routes
