1305d6e3aafc341862899eb9da3d33e092d619fe
Woo Jia Hao <woojiahao1234@gmail.com>
2018-08-11 19:50:02 +0800

    moved navigation arrows to the bottom

--- app/routes.py
+++ app/routes.py
@@ -13,13 +13,23 @@ def home():
 def effects():
 	return render_template('effects.html', page='Effects of declining population')
 
+
 @app.route('/hypothesis')
 def hypothesis():
-	return render_template('hypothesis.html', page='Hypothesis')\
+	return render_template('hypothesis.html', page='Hypothesis')
+
 
 @app.route('/live-births-rate')
 def live_births_rate():
 	return render_template('live_births.html', page='Live Births Rate')
+
+
+@app.route('/correlation_live_birth_enrolment')
+def correlation_live_birth_enrolment():
+	return render_template(
+		'correlation_live_birth_enrolment.html',
+		page='Correlation between live birth rate and primary enrolment')
+
 
 @app.route('/setup')
 def setup():
