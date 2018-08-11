0ececc73c7e3569f21174ae5cac8355ff7b9dfdb
Woo Jia Hao <woojiahao1234@gmail.com>
2018-08-11 10:26:18 +0800

    no longer supporting plotly due to api key being tedious kappa

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
-from app import app
 from flask import render_template
+
+from app import app
+
 
 @app.route('/')
 @app.route('/home')
 def home():
 	return render_template('home.html', page='Home')
+
 
 @app.route('/about')
 def about():
