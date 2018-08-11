84553880e83951a19b30e92a06f6cd4008890eb7
Woo Jia Hao <woojiahao1234@gmail.com>
2018-08-11 15:14:24 +0800

    moved each analysis to a new screen and implemented nav buttons

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,27 @@
-import pygal
-from flask import render_template, url_for
+from flask import render_template
 
 from app import app
 from app import db
 from plotting.enrolment import Enrolment, Genders
 
+
 # todo: render all the plots and save them, then use the saved plots instead of rendering everytime
 
 @app.route('/')
 @app.route('/home')
-@app.route('/analysis')
 def home():
-	enrolment = Enrolment(db.engine)
-
-	return render_template(
-		'home.html',
-		page='Home',
-		enrolment_line_male=enrolment.plot_line(Genders.MALE))
+	return render_template('home.html', page='Home')
 
 
 @app.route('/about')
 def about():
 	return render_template('about.html', page='About')
 
-@app.route('/processing/<switch>')
-def processing(switch):
-	enrolment = Enrolment(db.engine)
-	if switch == 'MALE':
-		return enrolment.plot_line(Genders.MALE).render_data_uri()
-	elif switch == 'FEMALE':
-		return enrolment.plot_line(Genders.FEMALE).render_data_uri()
 
+@app.route('/effects')
+def effects():
+	enrolment = Enrolment(db.engine)
+	return render_template(
+		'effects.html',
+		page='Effects',
+		enrolment_line_male=enrolment.plot_line(Genders.MALE))
